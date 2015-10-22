53b3d1864e61eac974ce6e487372b3d36e22c03f
Lin Jingke <linjk121@163.com>
2015-10-22 10:38:58 +0800

    add LocationAtShop to add or edit an item's shop location

--- JimGroceryDude/Controller/LocationAtShop/LocationAtShopVC.h
+++ JimGroceryDude/Controller/LocationAtShop/LocationAtShopVC.h
@@ -1,15 +1,18 @@
 //
 //  LocationAtShopVC.h
-//  Grocery Dude
+//  JimGroceryDude
 //
-//  Created by Tim Roadley on 24/12/12.
-//  Copyright (c) 2012 Tim Roadley. All rights reserved.
+//  Created by LinJK on 22/10/15.
+//  Copyright (c) 2015 LinJK. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "CoreDataHelper.h"
+
 @interface LocationAtShopVC : UIViewController <UITextFieldDelegate>
+
 @property (strong, nonatomic) NSManagedObjectID *selectedObjectID;
 @property (strong, nonatomic) IBOutlet UITextField *nameTextField;
+
 @end
 
--- JimGroceryDude/Controller/LocationAtShop/LocationsAtShopTVC.h
+++ JimGroceryDude/Controller/LocationAtShop/LocationsAtShopTVC.h
@@ -1,9 +1,9 @@
 //
 //  LocationsAtShopTVC.h
-//  Grocery Dude
+//  JimGroceryDude
 //
-//  Created by Tim Roadley on 24/12/12.
-//  Copyright (c) 2012 Tim Roadley. All rights reserved.
+//  Created by LinJK on 22/10/15.
+//  Copyright (c) 2015 LinJK. All rights reserved.
 //
 
 #import "CoreDataTVC.h"
