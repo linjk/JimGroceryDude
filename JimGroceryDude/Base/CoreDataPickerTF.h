e8710390e3d65e2de28d76cf999f639b37046d4d
Lin Jingke <linjk121@163.com>
2015-10-22 10:45:21 +0800

    新建CoreDataPickerTF基类

---
+++ JimGroceryDude/Base/CoreDataPickerTF.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataPickerTF.h
+//  JimGroceryDude
+//
+//  Created by hodi on 10/22/15.
+//  Copyright © 2015 LinJK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoreDataPickerTF : UITextField
+
+@end
---
+++ JimGroceryDude/Base/CoreDataPickerTF.m
@@ -0,0 +1,21 @@
+//
+//  CoreDataPickerTF.m
+//  JimGroceryDude
+//
+//  Created by hodi on 10/22/15.
+//  Copyright © 2015 LinJK. All rights reserved.
+//
+
+#import "CoreDataPickerTF.h"
+
+@implementation CoreDataPickerTF
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
